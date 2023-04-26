e0cdc3ce1ca4a8a3bbfd42a23f0488aa80bacf87
JasonChiu2 <stemlads@gmail.com>
2023-04-26 10:07:47 -0400

    Update

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
-const authRoute = require(`./routes/auth`);
-const port = process.env.PORT || 3000; //use external server port OR local 3000
 
+//Import Routes
+const authRoute = require(`./routes/auth`);
+
+//use external server port OR local 3000
+const port = process.env.PORT || 3000; 
+
+//Route Middlewares
 app.use(`/api/user`, authRoute);
 
 app.listen(3000, () => console.log(`Server is twerking on ${port}`));
