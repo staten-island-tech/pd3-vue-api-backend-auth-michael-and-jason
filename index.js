e2ebf3a0bf425e6886e16a64e32a60cf3534cb4e
michaelv24 <mvsch20@gmail.com>
2023-05-01 10:06:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const authRoute = require(`./routes/auth`);
 
 //use external server port OR local 3000
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
 
 //Route Middlewares
 app.use(`/api/user`, authRoute);
